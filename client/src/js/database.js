186de0c2c79c3e28f5b635e92d557253defe25d1
jony0418 <78985884+jony0418@users.noreply.github.com>
2023-08-15 01:50:27 -0600

    adding the istall js corrections

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,22 +12,10 @@ const initdb = async () =>
     },
   });
 
-// Method to add content to the database
-export const putDb = async (content) => {
-  const db = await initdb();
-  const tx = db.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  await store.add({ content });
-  return tx.done;
-};
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => console.error('putDb not implemented');
 
-// Method to get all content from the database
-export const getDb = async () => {
-  const db = await initdb();
-  const tx = db.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  const allContent = await store.getAll();
-  return allContent.map(item => item.content);
-};
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => console.error('getDb not implemented');
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,31 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Save the event for later use
+  window.deferredPrompt = e;
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+  // Show the install button
+  butInstall.classList.remove('hidden', false);
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+butInstall.addEventListener('click', async () => {
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+    return;
+  }
+
+  // Display the prompt
+  promptEvent.prompt();
+
+  // Clear the saved prompt event
+  window.deferredPrompt = null;
+
+  // Hide the install button
+  butInstall.classList.toggle('hidden', true);
+});
+
+window.addEventListener('appinstalled', (event) => {
+  // Clear the saved prompt event
+  window.deferredPrompt = null;
+});
