aa10b4974c547a18306c48006f805fc04bd5639f
jony0418 <78985884+jony0418@users.noreply.github.com>
2023-08-15 08:36:27 -0600

    added README and made modifications

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,9 +26,23 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// Cache worker
+// Asset caching
+const assetCache = new CacheFirst({
+  cacheName: 'asset-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+      maxEntries: 50, // Limit the number of entries in the cache
+    }),
+  ],
+});
+
+// Cache images
 registerRoute(
-  ({ request }) => request.destination === 'worker',
+  ({ request }) => request.destination === 'image',
   assetCache
 );
 
@@ -43,13 +57,3 @@ registerRoute(
   ({ request }) => request.destination === 'script',
   assetCache
 );
-
-// Asset caching
-const assetCache = new CacheFirst({
-  cacheName: 'asset-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-  ],
-});
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -9,42 +9,26 @@ const initdb = async () =>
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
-  },
-});
+    },
+  });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Method to add content to the database
 export const putDb = async (content) => {
-  // // console.error('putDb not implemented');
-  // if (!content.id) {
-  //   content.id = generateUniqueId(); 
-  // }
+  const db = await openDB('jate', 1); 
+  const tx = db.transaction('jate', 'readwrite');
   
-
-  const jateDb = await openDB('jate', 1); 
-  const tx = jateDb.transaction('jate', 'readwrite'); 
   const store = tx.objectStore('jate');
-  const request = store.put({id: 1, value: content});
-  const result = await request; 
-  console.log('result.value', result.value); 
-  // return result; 
-}; 
+  await store.add({ content });
+  return tx.done;
+};
 
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => { 
-  // console.error('getDb not implemented');
-  //create a connection to the database and version we want to use
-  const jateDb = await openDB('jate', 1); 
-  //open up the desired object store
-  const tx = jateDb.transaction('jate', 'readonly'); 
-  //open up the desired object store
+// Method to get all content from the database
+export const getDb = async () => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  //use the .getAll() method to get all data in the database
-  const request = store.get(1); 
-  //get confirmation of the request
-  const result = await request; 
-  console.log('result.value', result); 
-  return result?.value; 
-}; 
+  const allContent = await store.getAll();
+  return allContent.map(item => item.content);
+};
 
 initdb();
